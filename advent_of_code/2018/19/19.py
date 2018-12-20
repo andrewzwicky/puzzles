8dc68499dfa0985bde899682b5b4f03e7247a5ee
Andrew Zwicky <andrew.zwicky@gmail.com>
2018-12-20 14:27:43 -0600

    Updating 19

--- advent_of_code/2018/19/19.py
+++ advent_of_code/2018/19/19.py
@@ -1,15 +1,119 @@
-from pathlib import Path
-import itertools
-from collections import Counter, namedtuple, defaultdict
-from pprint import pprint
 import re
-from datetime import datetime
-import numpy as np
-import functools
-from copy import copy, deepcopy
-from enum import Enum, IntEnum
+from pathlib import Path
 
 data = Path('input.txt').read_text().splitlines()
-data = Path('test.txt').read_text().splitlines()
 
-print()
+
+# data = Path('test.txt').read_text().splitlines()
+
+
+def addr(a, b, c, regs):
+    regs[c] = regs[a] + regs[b]
+    return regs
+
+
+def addi(a, b, c, regs):
+    regs[c] = regs[a] + b
+    return regs
+
+
+def mulr(a, b, c, regs):
+    regs[c] = regs[a] * regs[b]
+    return regs
+
+
+def muli(a, b, c, regs):
+    regs[c] = regs[a] * b
+    return regs
+
+
+def banr(a, b, c, regs):
+    regs[c] = regs[a] & regs[b]
+    return regs
+
+
+def bani(a, b, c, regs):
+    regs[c] = regs[a] & b
+    return regs
+
+
+def borr(a, b, c, regs):
+    regs[c] = regs[a] | regs[b]
+    return regs
+
+
+def bori(a, b, c, regs):
+    regs[c] = regs[a] | b
+    return regs
+
+
+def setr(a, _, c, regs):
+    regs[c] = regs[a]
+    return regs
+
+
+def seti(a, _, c, regs):
+    regs[c] = a
+    return regs
+
+
+def gtir(a, b, c, regs):
+    regs[c] = 1 if (a > regs[b]) else 0
+    return regs
+
+
+def gtri(a, b, c, regs):
+    regs[c] = 1 if (regs[a] > b) else 0
+    return regs
+
+
+def gtrr(a, b, c, regs):
+    regs[c] = 1 if (regs[a] > regs[b]) else 0
+
+
+def eqir(a, b, c, regs):
+    regs[c] = 1 if (a == regs[b]) else 0
+
+
+def eqri(a, b, c, regs):
+    regs[c] = 1 if (regs[a] == b) else 0
+
+
+def eqrr(a, b, c, regs):
+    regs[c] = 1 if (regs[a] == regs[b]) else 0
+
+
+data_iter = iter(data)
+ip = int(re.search('\d+', next(data_iter)).group(0))
+
+instructions = []
+for i in data_iter:
+    name, *args = i.split(' ')
+    args = list(map(int, args))
+    instructions.append((eval(name), args))
+
+registers = [0, 0, 0, 0, 0, 0]
+
+while True:
+    instruction = registers[ip]
+    try:
+        func, args = instructions[instruction]
+        func(*args, registers)
+        registers[ip] += 1
+    except IndexError:
+        break
+
+print(registers[0])
+
+registers = [1, 0, 0, 0, 0, 0]
+
+while True:
+    instruction = registers[ip]
+    try:
+        func, args = instructions[instruction]
+        func(*args, registers)
+        registers[ip] += 1
+    except IndexError:
+        break
+
+print(registers[0])
